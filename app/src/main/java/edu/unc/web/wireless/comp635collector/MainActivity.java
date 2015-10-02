958bddc64175d374edfd40affd0fe6e31ecd8264
Alex Parker <alex.parker215@gmail.com>
2015-10-01 21:04:58 -0400

    Added connection type and upload/download speed functionality

--- app/src/main/java/edu/unc/web/wireless/comp635collector/MainActivity.java
+++ app/src/main/java/edu/unc/web/wireless/comp635collector/MainActivity.java
@@ -1,16 +1,121 @@
 package edu.unc.web.wireless.comp635collector;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.TrafficStats;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.SystemClock;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.session.AppKeyPair;
+
+import java.io.File;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends Activity {
+
+    final static private String APP_KEY = "qielk8mjvk131u9";
+    final static private String APP_SECRET = "d4g8n9hu8rune6d";
+    private DropboxAPI<AndroidAuthSession> mDBApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppKeyPair appKeys = new AppKeyPair(APP_KEY, APP_SECRET);
+        AndroidAuthSession session = new AndroidAuthSession(appKeys);
+        mDBApi = new DropboxAPI<AndroidAuthSession>(session);
+        mDBApi.getSession().startOAuth2Authentication(getApplicationContext());
+
+        Button getDataBtn = (Button) findViewById(R.id.button);
+        getDataBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String networkClass = getNetworkClass(getApplicationContext());
+                System.out.println("Connection type: " + networkClass);
+                //double uploadSpeed = getUploadSpeed();
+                try {
+                    getDownloadSpeed();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+        if (mDBApi.getSession().authenticationSuccessful()) {
+            try {
+                // Required to complete auth, sets the access token on the session
+                mDBApi.getSession().finishAuthentication();
+
+                String accessToken = mDBApi.getSession().getOAuth2AccessToken();
+            } catch (IllegalStateException e) {
+                Log.i("DbAuthLog", "Error authenticating", e);
+            }
+        }
+    }
+
+    public static String getNetworkClass(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        if(info==null || !info.isConnected())
+            return "-"; //not connected
+        if(info.getType() == ConnectivityManager.TYPE_WIFI)
+            return "WIFI";
+        if(info.getType() == ConnectivityManager.TYPE_MOBILE){
+            int networkType = info.getSubtype();
+            switch (networkType) {
+                case TelephonyManager.NETWORK_TYPE_GPRS:
+                case TelephonyManager.NETWORK_TYPE_EDGE:
+                case TelephonyManager.NETWORK_TYPE_CDMA:
+                case TelephonyManager.NETWORK_TYPE_1xRTT:
+                case TelephonyManager.NETWORK_TYPE_IDEN: //api<8 : replace by 11
+                    return "2G";
+                case TelephonyManager.NETWORK_TYPE_UMTS:
+                case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                case TelephonyManager.NETWORK_TYPE_HSDPA:
+                case TelephonyManager.NETWORK_TYPE_HSUPA:
+                case TelephonyManager.NETWORK_TYPE_HSPA:
+                case TelephonyManager.NETWORK_TYPE_EVDO_B: //api<9 : replace by 14
+                case TelephonyManager.NETWORK_TYPE_EHRPD:  //api<11 : replace by 12
+                case TelephonyManager.NETWORK_TYPE_HSPAP:  //api<13 : replace by 15
+                    return "3G";
+                case TelephonyManager.NETWORK_TYPE_LTE:    //api<11 : replace by 13
+                    return "4G";
+                default:
+                    return "?";
+            }
+        }
+        return "?";
+    }
+
+    public void getUploadSpeed() throws ExecutionException, InterruptedException {
+        File sdCard = Environment.getExternalStorageDirectory();
+        File uploadFile = new File(sdCard, "On_Guard.pdf");
+        UploadToDropbox upload = new UploadToDropbox(this, mDBApi, "/", uploadFile);
+        upload.execute();
+    }
+
+    public void getDownloadSpeed() throws ExecutionException, InterruptedException {
+        DownloadFromDropbox downloadFile = new DownloadFromDropbox(this, mDBApi, "/VMware.exe", Environment.getExternalStorageDirectory().getPath() + "/VMware.exe");
+        downloadFile.execute();
     }
 
     @Override
